b8f2f85beefe61f84258bc4e0b75d267e8c59c45
elbec <elbe.christopher@gmail.com>
2020-02-19 23:40:56 +0100

    fixed recorder warnings

--- TimeTracker/Recorder.cs
+++ TimeTracker/Recorder.cs
@@ -4,20 +4,15 @@ namespace TimeTracker
 {
     public struct Recorder
     {
-        public DateTime StartTime;
-        public DateTime EndTime;
-        public bool isTimerRunning;
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public bool IsTimerRunning { get; set; }
 
         public TimeSpan getTotalDuration()
         {
-            if (StartTime != null)
-            {
-                var start = StartTime;
-                var stop = EndTime;
-
-                return stop.Subtract(start).StripMilliseconds();
-            } else
-                return TimeSpan.FromMinutes(0);
+            var start = StartTime;
+            var stop = EndTime;
+            return stop.Subtract(start).StripMilliseconds();
         }
     }
 }
